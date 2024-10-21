c2ba6cfdaded5739a6ab29aca7dad340fbc20bd0
Ben <befa3400@gmail.com>
2024-10-21 18:56:47 +1100

    mini calender

---
+++ index.js
@@ -0,0 +1,16 @@
+const monthName = document.getElementById("month-name");
+const dayNameEl = document.getElementById("day-name");
+const dayNum = document.getElementById("day-number");
+const yearEl = document.getElementById("year");
+
+const date = new Date();
+const month = date.getMonth();
+monthName.innerText = date.toLocaleDateString("en",{
+    month:"long"
+})
+dayNameEl.innerText = date.toLocaleDateString("en", {
+    weekday:"long"
+})
+
+dayNumEl.innerText = date.getDate();
+yearEl.innerText = date.getFullYear();
